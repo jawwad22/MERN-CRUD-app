a145a57a67f67d2be093aee5715a0cb8f9084b10
unknown <mjawwadiqbal22@gmail.com>
2019-03-30 15:55:32 -0700

    added Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+//Component 
+import TodosList from "./components/todos-list-component";
+import EditTodo from "./components/edit-todos-component";
+import CreateTodo from "./components/create-todo-component"
+
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h2>MERN-Stact Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a href="" className="navbar-brand">
+        {/* //logo here */}
+        <Link to="/" className="navbar-brand">MERN Stack Todo App</Link>
+      </a>
+          </nav>
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
         </div>
-        <Route path="/" exact component={TodosList} />
-        <Route path="/edit/:id" component={EditTodo} />
-        <Route path="/create" component={CreateTodo} />
       </Router>
     );
   }
