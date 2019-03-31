20c2c8407c2ba183a5739310181cce7b9de78a4f
unknown <mjawwadiqbal22@gmail.com>
2019-03-30 21:19:00 -0700

    --Todo Create

--- src/components/create-todo-component.js
+++ src/components/create-todo-component.js
@@ -1,6 +1,43 @@
 import React, { Component } from 'react'
 
 export default class CreateTodo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo_description: '',
+      todo_responsible: '',
+      todo_priority: '',
+      todo_completed: false
+    }
+  }
+  onChangeTodoDescription(e){
+    this.setState({
+      todo_description:e.target.value
+    })
+  }
+
+  onChangeTodoResponsible(e){
+    this.setState({
+      todo_responsible:e.target.value
+    })
+  }
+  onChangeTodoPriority(e){
+    this.setState({
+      todo_priority:e.target.value
+    })
+  }
+  onSubmit(e){
+   e.preventDefault();
+
+    console.log('form Submit');
+    console.log(this.state)
+   this.setState({
+    todo_description: '',
+    todo_responsible: '',
+    todo_priority: '',
+    todo_completed: false
+   })
+  }
   render() {
     return (
       <div>
