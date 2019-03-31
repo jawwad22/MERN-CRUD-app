e598ddf26c34585929b6bb37e9486e4488d9045e
unknown <mjawwadiqbal22@gmail.com>
2019-03-30 21:12:04 -0700

    --added logo

--- src/App.js
+++ src/App.js
@@ -7,18 +7,31 @@ import TodosList from "./components/todos-list-component";
 import EditTodo from "./components/edit-todos-component";
 import CreateTodo from "./components/create-todo-component"
 
+import logo from './logo.png'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
+          
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a href="" className="navbar-brand">
-        {/* //logo here */}
-        <Link to="/" className="navbar-brand">MERN Stack Todo App</Link>
-      </a>
+            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
           </nav>
+
           <Route path="/" exact component={TodosList} />
           <Route path="/edit/:id" component={EditTodo} />
           <Route path="/create" component={CreateTodo} />
